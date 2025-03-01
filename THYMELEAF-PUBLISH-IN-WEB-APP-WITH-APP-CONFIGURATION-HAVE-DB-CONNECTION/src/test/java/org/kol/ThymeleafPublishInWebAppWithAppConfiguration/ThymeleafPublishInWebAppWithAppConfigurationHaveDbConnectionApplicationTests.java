648a0816dc469e21964c4756e33ebcdc36b0ec77
iSemicolon <palash.samanta7497@gmail.com>
2025-03-01 20:56:57 +0530

    "Springboot with azure app-configuration which holding db connection"

---
+++ THYMELEAF-PUBLISH-IN-WEB-APP-WITH-APP-CONFIGURATION-HAVE-DB-CONNECTION/src/test/java/org/kol/ThymeleafPublishInWebAppWithAppConfiguration/ThymeleafPublishInWebAppWithAppConfigurationHaveDbConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kol.ThymeleafPublishInWebAppWithAppConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPublishInWebAppWithAppConfigurationHaveDbConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
